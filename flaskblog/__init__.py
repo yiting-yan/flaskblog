805734d85bd13b46ee53895dec052693c13b9d39
YufeiZhan <yufeizhan@YufeiZhans-MacBook-Pro.local>
2020-07-27 23:29:47 +0800

    Added Rich Text Editor and Modified View of posts on Home page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_ckeditor import CKEditor # Rich text editor
 
 app = Flask(__name__)
+ckeditor = CKEditor(app) # Rich text editor
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
