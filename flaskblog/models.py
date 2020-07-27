73d6901084f3cb32b100c0a192176c3bbc1aced6
YufeiZhan <yufeizhan@YufeiZhans-MacBook-Pro.local>
2020-07-28 03:12:10 +0800

    Fixed number of comment bug.

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -40,7 +40,7 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     likes = db.relationship('Like', backref='post', lazy='dynamic')
-    comments = db.relationship('Like', backref='comments', lazy='dynamic')
+    comments = db.relationship('Comment', backref='comments', lazy='dynamic')
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
