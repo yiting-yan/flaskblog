c23599ec9306302b3d0c8a2fe069bebf66f09d81
zoidhbzhj292 <zoidhbzhj292@a2plcpnl0179.prod.iad2.secureserver.net>
2020-07-27 22:27:26 -0700

    first commit on server.

---
+++ flaskblog/models.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.png')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='commenter', lazy=True)
+    liked = db.relationship( 'Like', backref='Liker', lazy=True)
+
+    def like_post(self, post):
+        if not self.has_liked_post(post):
+            like = Like(liker_id=self.id, post_id=post.id)
+            db.session.add(like)
+
+    def unlike_post(self, post):
+        if self.has_liked_post(post):
+            Like.query.filter_by(liker_id=self.id, post_id=post.id).delete()
+
+    def has_liked_post(self, post):
+        return Like.query.filter(Like.liker_id == self.id, Like.post_id == post.id).count() > 0
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    likes = db.relationship('Like', backref='post', lazy='dynamic')
+    comments = db.relationship('Comment', backref='comments', lazy='dynamic')
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    commenter_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    poster_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    def __repr__(self):
+        return f"Comment('{self.date_posted}',''{self.content}')"
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    liker_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
